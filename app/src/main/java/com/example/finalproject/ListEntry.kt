5a6f7883276c4f23868ce5b9734e013e26a25b71
keith <keithzhang021@outlook.com>
2023-05-10 16:06:00 -0400

    updated functions and added debug outputs

--- app/src/main/java/com/example/finalproject/ListEntry.kt
+++ app/src/main/java/com/example/finalproject/ListEntry.kt
@@ -4,36 +4,34 @@ import android.location.Location
 import java.util.*
 
 class ListEntry {
-    class DateEntry(var year: Int, var month: Int, var day: Int, var hour: Int, var minute: Int)
-
     var name: String
-    var date: Calendar = Calendar.getInstance()
+    var date: DateEntry
     var description: String
     var location: Location? = null
 
     constructor(name: String, date: DateEntry, description: String) {
         this.name = name
         this.description = description
-
-        setTime(date)
+        this.date = date
     }
 
-    constructor(name: String, date: DateEntry, description: String, location: Location) {
-        this.name = name
-        this.description = description
-        this.location = location
-
-        setTime(date)
+    fun setEventLocation (loc: Location) {
+        this.location = loc
     }
 
     fun setTime(date: DateEntry) {
-        this.date.set(date.year, date.month, date.day, date.hour, date.minute, 0)
+        this.date = date
     }
 
-    fun getTime(): String {
-        return String.format("%d%d%d",
-            date.get(Calendar.YEAR),
-            date.get(Calendar.MONTH),
-            date.get(Calendar.DAY_OF_MONTH))
+    fun getDate(): String {
+        return this.date.getDate()
+    }
+
+    fun getTime():String {
+        return this.date.getTime()
+    }
+
+    override fun toString(): String {
+        return "$name - $date - $description"
     }
 }
--- app/src/main/java/com/example/finalproject/TodoListModel.kt
+++ app/src/main/java/com/example/finalproject/TodoListModel.kt
@@ -2,19 +2,48 @@ package com.example.finalproject
 
 
 class TodoListModel {
-    var eventList: Map<String, ArrayList<ListEntry>> = HashMap()
+    var eventList: HashMap<String, ArrayList<ListEntry>> = HashMap()
     var count = 0
 
-    fun pushEvent(entry: ListEntry) {
-        var date_key = entry.getTime()
-        if (eventList.containsKey(date_key)) {
-
+    fun addEvent(entry: ListEntry) {
+        var dateString = entry.getTime()
+        if (this.eventList.containsKey(dateString)) {
+            this.eventList[dateString]!!.add(entry)
         } else {
-
+            this.eventList[dateString] = ArrayList()
+            this.eventList[dateString]!!.add(entry)
         }
+
+        this.eventList[dateString]!!.sortWith(compareBy { it.date.getTime() })
+
+        count += 1
     }
 
-    fun getDate() {
+    fun removeEvent(entry: ListEntry) {
+        var dateString = entry.getTime()
+        this.eventList[dateString]?.remove(entry)
+    }
 
+    fun updateEvent(oldEntry: ListEntry, newEntry: ListEntry) {
+        this.removeEvent(oldEntry)
+        this.addEvent(newEntry)
+    }
+
+    fun getDate(date: DateEntry): ArrayList<ListEntry>? {
+        val dateString = date.getDate()
+        return this.eventList[dateString]
+    }
+
+    override fun toString(): String {
+        var res = ""
+        this.eventList.forEach { it1 ->
+            var temp = "========\n$it1.key\n"
+            it1.value.forEach {it2 ->
+                temp += "    $it2\n"
+            }
+            res += temp + "\n"
+        }
+
+        return res
     }
 }
