ee867b019176921a7ba673c348a0e95779af4b6e
keith <keithzhang021@outlook.com>
2023-05-10 16:05:20 -0400

    updated functions and added debug outputs

--- app/src/main/java/com/example/finalproject/DateEntry.kt
+++ app/src/main/java/com/example/finalproject/DateEntry.kt
@@ -1,3 +1,16 @@
 package com.example.finalproject
 
-class DateEntry(var year: Int, var month: Int, var day: Int, var hour: Int, var minute: Int)
+class DateEntry(var year: Int, var month: Int, var day: Int, var hour: Int, var minute: Int) {
+    fun getDate(): String {
+        return String.format("%d%d%d", year, month, day)
+    }
+
+    fun getTime(): String {
+        return String.format("%02d%d", hour, minute)
+    }
+
+    override fun toString(): String {
+        return String.format("%d-%d-%d | %02d:%d",
+            year, month, day, hour, minute)
+    }
+}
